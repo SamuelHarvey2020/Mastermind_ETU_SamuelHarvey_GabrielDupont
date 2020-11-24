7a170bcfbff4b52240e817282353826440b4bc7a
Samuel Harvey <shdrawings@outlook.com>
2020-11-24 15:50:30 -0500

    Continuité de la liste

--- Code/ListeDouble.h
+++ Code/ListeDouble.h
@@ -17,10 +17,12 @@ public :
 	~ListeDouble();
 	void vider();
 
-	bool ajouter(T* _livre);
-	bool retirer(T* _livre);
+	bool ajouter(T* element);
+	void retirer(Noeud <T>* noeudCourant);
+
+	const int getNbElements();
 	Noeud<T>* getPremierNoeud();
-	void affiche();
+	const bool isEmpty();
 
 	typedef Iterateur<T> iterateur;
 	Iterateur<T> begin();
@@ -28,14 +30,14 @@ public :
 
 private:
 	Noeud* premierNoeud;
-private:
-	
+	void retirer();
 };
 
 //TODO : Implémentez votre liste double (toutes les méthodes de la classe mère)
 template <class T>
 ListeDouble<T>::ListeDouble()
 {
+	premierNoeud = NULL;
 }
 
 template <class T>
@@ -49,36 +51,108 @@ void ListeDouble<T>::vider()
 }
 
 template<class T>
-inline bool ListeDouble<T>::ajouter(T* _livre)
+inline bool ListeDouble<T>::ajouter(T* element)
 {
-	return false;
+	Noeud<T>* nouveauNoeud = new Noeud;
+	nouveauNoeud->setElement(element);
+	bool canBeAdded = true;
+
+	if (this->premierNoeud == NULL) {
+		this->premierNoeud = nouveauNoeud;
+		this->premierNoeud->setSuivant(NULL);
+	}
+	else {
+
+		if (*element < *(premierNoeud->getElement())) {
+			nouveauNoeud->setSuivant(premierNoeud);
+			this->premierNoeud = nouveauNoeud;
+		}
+		else {
+
+			Noeud<T>* noeudTemporaire = this->premierNoeud;
+			//Iterateur<T> iter(noeudTemporaire);
+			bool solved = false;
+
+			if (*element != *(noeudTemporaire->getElement())) {
+
+				while (solved != true) {
+
+					if (noeudTemporaire->getSuivant() == NULL) {
+						noeudTemporaire->setSuivant(nouveauNoeud);
+						noeudTemporaire->getSuivant()->setSuivant(NULL);
+						solved = true;
+					}
+					else if (*element == *(noeudTemporaire->getSuivant()->getElement())) {
+
+						canBeAdded = false;
+						break;
+					}
+					else if (*element < *(noeudTemporaire->getSuivant()->getElement())) {
+
+						nouveauNoeud->setSuivant(noeudTemporaire->getSuivant());
+						noeudTemporaire->setSuivant(nouveauNoeud);
+						solved = true;
+					}
+					else {
+						iterateur++;
+					}
+				}
+			}
+			else {
+				canBeAdded = false;
+			}
+
+		}
+
+	}
+	return canBeAdded;
 }
 
 template<class T>
-inline bool ListeDouble<T>::retirer(T* _livre)
+inline void ListeDouble<T>::retirer(Noeud <T>* noeudCourant)
 {
-	return false;
+	Noeud<T>* noeudTemporaire = noeudCourant;
+	noeudTemporaire->getPrecedent()->setSuivant(noeudTemporaire->getSuivant());
+	noeudTemporaire->getSuivant()->setPrecedent(noeudTemporaire->getPrecedent());
+	delete noeudTemporaire;
+}
+
+template<class T>
+inline const int ListeDouble<T>::getNbElements()
+{
+	return 0;
 }
 
 template<class T>
 inline Noeud<T>* ListeDouble<T>::getPremierNoeud()
 {
-	return NULL;
+	return this->premierNoeud;
 }
 
 template<class T>
-inline void ListeDouble<T>::affiche()
+inline const bool ListeDouble<T>::isEmpty()
 {
+	if (this->premierNoeud == NULL;) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 template<class T>
 inline Iterateur<T> ListeDouble<T>::begin()
 {
-	return Iterateur<T>();
+	return Iterateur<T>(premierNoeud);
 }
 
 template<class T>
 inline Iterateur<T> ListeDouble<T>::end()
 {
-	return Iterateur<T>();
+	return Iterateur<T>(NULL);
+}
+
+template<class T>
+inline void ListeDouble<T>::retirer()
+{
 }
--- TestsCode/liste.cpp
+++ TestsCode/liste.cpp
@@ -4,10 +4,63 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+/*
 namespace Tests_Liste
 {		
-	TEST_CLASS(liste)
+	//TEST DE CRÉATION DE LISTE
+	TEST_METHOD(UneListeDevraitEtreCrééeAvecPremierNoeudNull)
 	{
+		ListeDouble<int> liste;
 
-	};
+		Assert::IsTrue(l.getPremierNoeud() == NULL);
+	}
+
+	//====================================TESTS DE LA MÉTHODE AJOUTER============================================================
+
+	TEST_METHOD(OnDevraitPouvoirAjouterUnLivreEtUnNoeudAUneListeVide)
+	{
+		Liste l;
+		Livre* _livre = new Livre(1, "test", 2000);
+
+		Assert::IsTrue(l.ajouter(_livre));
+		Assert::IsTrue(l.getPremierNoeud() != NULL);
+	}
+
+	TEST_METHOD(OnDevraitPouvoirAjouterUnLivreEtUnNoeudAUneListeNonVide)
+	{
+		Liste l;
+		Livre* _livre = new Livre(1, "test", 2001);
+		Livre* _livre2 = new Livre(2, "test", 2002);
+		l.ajouter(_livre);
+
+		Assert::IsTrue(l.ajouter(_livre2));
+	}
+
+
+	TEST_METHOD(OnNeDevraitPasPouvoirAjouterUnLivreSiLeLivreEstDéjaPrésent)
+	{
+		Liste l;
+		Livre* _livre = new Livre(1, "test", 2000);
+		Livre* _livre2 = new Livre(1, "test", 2000);
+		l.ajouter(_livre);
+
+		Assert::IsFalse(l.ajouter(_livre2));
+	}
+
+
+	TEST_METHOD(OnDevraitPouvoirEffectuerUnChainageCorrect)
+	{
+		Liste l;
+		Livre* _livre = new Livre(10, "test", 2000);
+		Livre* _livre2 = new Livre(5, "test", 2000);
+		Livre* _livre3 = new Livre(7, "test", 2000);
+		l.ajouter(_livre);
+		l.ajouter(_livre2);
+		l.ajouter(_livre3);
+
+		Assert::IsTrue(l.getPremierNoeud()->getLivre() == _livre2);
+		Assert::IsTrue(l.getPremierNoeud()->getSuivant()->getLivre() == _livre3);
+	}
+
 }
+*/
